1a09cf9ac43f33f7d6995224a4663bcd19415b4f
Gbolahan Coker <gbolahancoker99@gmail.com>
2026-04-24 11:55:41 +0100

    f

--- types/express.d.ts
+++ types/express.d.ts
@@ -1,5 +1,17 @@
 import "express";
 
+// with this i can do a auth check in every module i want without importing the interface every time
+// for example in messageController.ts i can do req.user?.userId without importing the AuthRequest interface
+// declare global {
+//   namespace Express {
+//     interface Request {
+//       user?: {
+//         userId: string;
+//         name: string;
+//       };
+//     }
+//   }
+// }
 declare module "express-serve-static-core" {
   interface Request {
     user?: {
