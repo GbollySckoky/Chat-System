76bc553c0a26df759297d3fed19f995fb9e26893
Gbolahan Coker <gbolahancoker99@gmail.com>
2026-05-02 22:53:25 +0100

    hh

--- route/notification.ts
+++ route/notification.ts
@@ -22,7 +22,7 @@ const router = Router()
  *         content:
  *           application/json:
  *             schema:
- *               $ref: '#/components/schemas/Notification'
+ *               $ref: '#/components/schemas/notifications'
  *       500:
  *         description: Internal server error
  */
